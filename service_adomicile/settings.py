7fd70ca81e26ac234ea287d8659cdb35f8f6d09b
ali <zewdew69@gmail.com>
2026-05-07 20:26:53 +0100

    Deploy config

--- service_adomicile/settings.py
+++ service_adomicile/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f7l$!t)8qhr*0q9if#0n4mskcig=(kyz1g!$n4al27*(8kn5xd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
